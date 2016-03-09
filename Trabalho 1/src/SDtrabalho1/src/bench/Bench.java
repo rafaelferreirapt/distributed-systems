c0f36fec915e3dcb69dff2cae29c95fa1dbe2e8e
Rafael Ferreira <mail@rafaelferreira.pt>
2016-03-09 14:11:34 +0000

    bench implemented

--- Trabalho 1/src/SDtrabalho1/src/bench/Bench.java
+++ Trabalho 1/src/SDtrabalho1/src/bench/Bench.java
@@ -4,14 +4,28 @@
  */
 package bench;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author António Ferreira, 67405; Rodrigo Cunha, 67800
  */
 public class Bench implements IReferee, ICoach, IContestant{
     
-    public Bench(){
+    private int nContestantsInBenchTeamA, nContestantsInBenchTeamB;
+    private int nContestantsTeamA, nContestantsTeamB;
     
+    private int nContestantsSelectedA = 0, nContestantsSelectedB = 0;
+    private boolean calledByCoachA = false, followCoachA = false;
+    private boolean calledByCoachB = false, followCoachB = false;
+    private static int nContestantsSelectedMax = 3;
+    
+    private static boolean trialDecisionTaken = false, callTrialTaken = false;
+            
+    public Bench(int nContestantsTeamA, int nContestantsTeamB){
+        this.nContestantsTeamA = this.nContestantsInBenchTeamA = nContestantsTeamA;
+        this.nContestantsTeamB = this.nContestantsInBenchTeamB= nContestantsTeamB;
     }
     
     /* REFEREE METHODS */
@@ -22,15 +36,19 @@ public class Bench implements IReferee, ICoach, IContestant{
      */
     @Override
     public synchronized void callTrial() {
-        // signalForCallTrial
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        callTrialTaken = true;
+        notifyAll();
     }
     
     @Override
     public synchronized void waitForCallTrial(){
-        // to the coach
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        
+        while(!callTrialTaken){
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Bench.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     /**
@@ -40,12 +58,18 @@ public class Bench implements IReferee, ICoach, IContestant{
      */
     @Override
     public synchronized void assertTrialDecision() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        trialDecisionTaken = true;
     }
 
     @Override
     public synchronized void waitForAssertTrialDecision(){
-        
+        while(!trialDecisionTaken){
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Bench.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
     
     /* COACH METHODS */
@@ -54,21 +78,62 @@ public class Bench implements IReferee, ICoach, IContestant{
      * In coach life cycle, transition between "watch trial" and "wait for referee command"
      */
     @Override
-    public void reviewNotes() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public synchronized void reviewNotes(String team) {
+        if(team.equals("A")){
+            while(this.nContestantsInBenchTeamA != 5){
+                try {
+                    wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Bench.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }else if(team.equals("B")){
+            while(this.nContestantsInBenchTeamB != 5){
+                try {
+                    wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Bench.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
     }
 
     /**
      * In coach life cycle, transition between "wait for referee command" and "assemble team"
      */
     @Override
-    public synchronized void callContestants() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public synchronized void callContestants(String team) {
+        if(team.equals("A")){
+            this.calledByCoachA = true;   
+        }else if(team.equals("B")){
+            this.calledByCoachB = true;
+        }
+        notifyAll();
     }
     
     @Override
-    public synchronized void waitForCallContestants(){
-        
+    public synchronized void waitForCallContestants(String team){
+        if(team.equals("A")){
+            while(!this.calledByCoachA || this.nContestantsSelectedA == nContestantsSelectedMax){
+                try {
+                    wait();
+                    this.nContestantsSelectedA++;
+                    this.nContestantsInBenchTeamA--;
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Bench.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }else if(team.equals("B")){
+            while(!this.calledByCoachB || this.nContestantsSelectedB == nContestantsSelectedMax){
+                try {
+                    wait();
+                    this.nContestantsSelectedB++;
+                    this.nContestantsInBenchTeamB--;
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Bench.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
     }
     
     /* PLAYERS METHODS */
@@ -79,13 +144,39 @@ public class Bench implements IReferee, ICoach, IContestant{
      * In Contestants life cycle, transition between "seat at the bench" and "stand in position"
      */
     @Override
-    public synchronized void followCoachAdvice() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public synchronized void followCoachAdvice(String team) {
+        if(team.equals("A")){
+            if(this.nContestantsSelectedA == 3){
+                this.followCoachA = true;
+                notifyAll();
+            }
+        }else if(team.equals("B")){
+            if(this.nContestantsSelectedB == 3){
+                this.followCoachB = true;
+                notifyAll();
+            }
+        }
     }
     
     @Override
-    public synchronized void waitForFollowCoachAdvice(){
-        
+    public synchronized void waitForFollowCoachAdvice(String team){
+        if(team.equals("A")){
+            while(!this.followCoachA){
+                try {
+                    wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Bench.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }else if(team.equals("B")){
+            while(!this.followCoachB){
+                try {
+                    wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Bench.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
     }
     
     /**
@@ -94,8 +185,14 @@ public class Bench implements IReferee, ICoach, IContestant{
      * callContestants by their coaches if they are selected to join the next trial
      */
     @Override
-    public void seatDown() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public synchronized void seatDown(String team){
+        if(team.equals("A")){
+            this.nContestantsInBenchTeamA++;
+            notifyAll();
+        }else if(team.equals("B")){
+            this.nContestantsInBenchTeamB++;
+            notifyAll();
+        }
     }
     
 }
