772b5b514df7e707e29ce3accba2d09b8a6fe59e
Rafael Ferreira <mail@rafaelferreira.pt>
2016-03-16 20:25:46 +0000

    remove aula 1

--- Aula 01/Rafael/Aula/src/ex1/ExceptionEx1.java
+++
@@ -1,8 +0,0 @@
-package ex1;
-
-public class ExceptionEx1 extends Exception {
-
-    public ExceptionEx1(String error){
-        super(error);
-    }
-}
--- Aula 01/Rodrigo/Aula01/src/ex1/FIFOObj.java
+++
@@ -1,7 +0,0 @@
-package ex1;
-
-public class FIFOObj {
-    private Object word;
-    private int realsize;
-    
-}
