63960dd523dd719632cf6e00c00c14c90bb91412
Rafael Ferreira <mail@rafaelferreira.pt>
2016-05-23 15:33:29 +0100

    wait

--- Trabalho 3/SDtrabalho3/launch_script/machines.py
+++ Trabalho 3/SDtrabalho3/launch_script/machines.py
@@ -149,9 +149,9 @@ def upload():
                 remote_path="Public/classes/",
                 recursive=True)
 
-        print Fore.LIGHTGREEN_EX + "cd Public/classes; unzip java.zip; mv java/* .; " \
-                                   "rm -rf java; rm java.zip" + Style.RESET_ALL
-        ssh.exec_command("cd Public/classes; unzip java.zip; mv java/* .;")  # rm -rf java; rm java.zip")
+        print Fore.LIGHTGREEN_EX + "cd Public/classes; unzip java.zip; mv java/* .; " + Style.RESET_ALL
+        stdin, stdout, stderr = ssh.exec_command("cd Public/classes; unzip java.zip; mv java/* .;")
+        stdout.channel.recv_exit_status()
 
         print Fore.LIGHTGREEN_EX + "Public/classes/config.ini" + Style.RESET_ALL
         scp.put(files="configs/config_up.ini", remote_path="Public/classes/config.ini")
